03aabc52309c150f3f66f9ec09a8934a08d3cd21
Josh Rogers <josh.rogers@catchco.com>
2021-10-27 16:57:09 -0500

    Initial commit

---
+++ AzFuncDomainBreakDemo/ExampleTriggers.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CatchCo.DomainBreakDemo
+{
+    [StorageAccount("Azure_Storage_ConnectionString")]
+    public static class ExampleTriggers
+    {
+        private const string CONTAINER_NAME = "test-container";
+        private const string TABLE_NAME = "TestTableA";
+        private const string QUEUE_NAME = "test-queue";
+
+        [FunctionName("ExampleTimerTrigger")]
+        public static void RunExampleTimerTrigger(
+            [TimerTrigger("0 * */2 * * *")] TimerInfo myTimer,
+            [Table(TABLE_NAME)] CloudTable table,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation("[{InvocationId}] C# Timer trigger function triggered", context.InvocationId);
+        }
+
+        [FunctionName("ExampleHttpTrigger")]
+        public static ActionResult RunExampleHttpTrigger(
+            [HttpTrigger(AuthorizationLevel.Function, "GET", Route = null)] HttpRequest req,
+            [Table(TABLE_NAME)] CloudTable table,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation("[{InvocationId}] C# HTTP trigger function triggered", context.InvocationId);
+            return new OkObjectResult("You called, we answered...");
+        }
+
+        [FunctionName("ExampleBlobTrigger")]
+        public static void RunBlobTrigger(
+            [BlobTrigger(CONTAINER_NAME)] CloudBlockBlob blob,
+            [Queue(QUEUE_NAME)] CloudQueue cloudQueue,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation("[{InvocationId}] C# Blob trigger function triggered", context.InvocationId);
+        }
+        
+        [FunctionName("ExampleQueueTrigger")]
+        public static void RunBlobTrigger([QueueTrigger(QUEUE_NAME)] CloudQueue queue,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation("[{InvocationId}] C# Queue trigger function triggered", context.InvocationId);
+        }
+    }
+}
